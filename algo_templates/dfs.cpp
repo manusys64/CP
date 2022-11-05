b0c86dfaaa8ff649301bcd2e750d9493885a9730
lavinraj <lavinrajmohan@gmail.com>
2022-11-05 12:43:57 +0530

    debug the segmentation fault

--- algo_templates/dfs.cpp
+++ algo_templates/dfs.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const int N = 1e7 + 5;
 
 //basic dfs structure
-void dfs (int vertex, vector<int> graph[], vector<int>& visited) {
+void dfs (int vertex, vector<int> graph[], vector<bool> visited) {
 	visited[vertex] = true;
 	cout << vertex << endl;
 	for (int child : graph[vertex]) {
@@ -16,13 +16,15 @@ void dfs (int vertex, vector<int> graph[], vector<int>& visited) {
 }
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	// Numuber of nodes in the given graph
 	int n;
 	cin >> n;
 
 	// Declaring adjacency list && visited array
-	vector<int> graph[n];
-	vector<int> visited(n);
+	vector<int> graph[n + 1];
+	vector<bool> visited(n + 1, false);
 
 	// Takig input of graph
 	for (int i = 0; i < n - 1; ++i) {
